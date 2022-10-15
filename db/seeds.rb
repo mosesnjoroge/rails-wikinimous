7bf83e78e89ac738699dc808e2e012fe27ff658c
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-10-15 16:11:00 -0400

    gem faker set-up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# TODO: Write a seed to insert 100 posts in the database
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs
+  )
+  article.save
+end
